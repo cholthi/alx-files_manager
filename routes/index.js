829189f9e6956a8752bea28ddd3efa3a984f83b9
cholthi <cholthipaul@gmail.com>
2024-05-18 12:51:29 +0300

    added FilesController.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesCOntroller';
 
 const router = Router();
 
@@ -12,4 +13,8 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+
 module.exports = router;
